4ec5158395328ee36205b93eee1bc1ca8da835d9
Xavier Hanin <xavier.hanin@gmail.com>
2013-01-12 15:33:02 +0100

    refactor basic question test to use theories, so that all questions are verified even when some are failing

--- src/test/java/cs13/handlers/BasicQuestionHandlerTest.java
+++ src/test/java/cs13/handlers/BasicQuestionHandlerTest.java
@@ -4,31 +4,38 @@ import cs13.util.ServerRule;
 import org.jboss.resteasy.client.ClientResponse;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Theories.class)
 public class BasicQuestionHandlerTest {
 
     @ClassRule
     public static ServerRule server = new ServerRule();
 
-    @Test
-    public void should_server_respond_to_basic_questions() throws Exception {
-        askBasicQuestionAndExpectAnswer("Quelle est ton adresse email", "xavier.hanin@gmail.com");
-        askBasicQuestionAndExpectAnswer("Es tu abonne a la mailing list(OUI/NON)", "OUI");
-        askBasicQuestionAndExpectAnswer("Es tu heureux de participer(OUI/NON)", "OUI");
-        askBasicQuestionAndExpectAnswer("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI");
-        askBasicQuestionAndExpectAnswer("As tu bien recu le premier enonce(OUI/NON)", "OUI");
-        askBasicQuestionAndExpectAnswer("Est ce que tu reponds toujours oui(OUI/NON)", "NON");
-        askBasicQuestionAndExpectAnswer("1+1", "2");
-        askBasicQuestionAndExpectAnswer("2+2", "4");
-        askBasicQuestionAndExpectAnswer("(1+2)/2", "1,5");
-        askBasicQuestionAndExpectAnswer("((1+2)+3+4+(5+6+7)+(8+9+10)*3)/2*5", "272,5");
-        askBasicQuestionAndExpectAnswer("1,5*4", "6");
-        askBasicQuestionAndExpectAnswer("((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000",
-                "31878018903828899277492024491376690701584023926880");
-        askBasicQuestionAndExpectAnswer("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)", "PAS_TOP");
+    @DataPoints
+    public static Question[] data() {
+        return new Question[] {
+            q("Quelle est ton adresse email", "xavier.hanin@gmail.com"),
+            q("Es tu abonne a la mailing list(OUI/NON)", "OUI"),
+            q("Es tu heureux de participer(OUI/NON)", "OUI"),
+            q("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI"),
+            q("As tu bien recu le premier enonce(OUI/NON)", "OUI"),
+            q("Est ce que tu reponds toujours oui(OUI/NON)", "NON"),
+            q("1+1", "2"),
+            q("2+2", "4"),
+            q("(1+2)/2", "1,5"),
+            q("((1+2)+3+4+(5+6+7)+(8+9+10)*3)/2*5", "272,5"),
+            q("1,5*4", "6"),
+            q("((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000",
+                    "31878018903828899277492024491376690701584023926880"),
+            q("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)", "PAS_TOP"),
+        };
     }
 
     @Test
@@ -43,12 +50,26 @@ public class BasicQuestionHandlerTest {
         assertThat(response.getResponseStatus().getStatusCode(), is(404));
     }
 
-    private void askBasicQuestionAndExpectAnswer(String question, String answer) throws Exception {
-        ClientResponse<String> response = server.request("/").queryParameter("q", question).get(String.class);
+    @Theory
+    public void should_server_respond_to_basic_question(Question q) throws Exception {
+        ClientResponse<String> response = server.request("/").queryParameter("q", q.question).get(String.class);
         assertThat(
-                "expected " + answer + " when asking " + question,
+                "expected " + q.answer + " when asking " + q.question,
                 response.getEntity(),
-                is(answer));
+                is(q.answer));
     }
 
+    private static Question q(String question, String answer) {
+        return new Question(question, answer);
+    }
+
+    private static class Question {
+        private String question;
+        private String answer;
+
+        private Question(String question, String answer) {
+            this.question = question;
+            this.answer = answer;
+        }
+    }
 }
