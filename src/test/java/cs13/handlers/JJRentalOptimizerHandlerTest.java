255e8fc5f94b1d3cf87900daf2def1e2b3bdc276
Xavier Hanin <xavier.hanin@gmail.com>
2013-01-15 17:39:47 +0100

    add more tests to jajascript rental handler, based on input provided

--- src/test/java/cs13/handlers/JJRentalOptimizerHandlerTest.java
+++ src/test/java/cs13/handlers/JJRentalOptimizerHandlerTest.java
@@ -61,4 +61,40 @@ public class JJRentalOptimizerHandlerTest {
         assertThat(json.get("path").get(1).asText(), is("skinny-superman-4"));
         assertThat(json.get("path").get(11).asText(), is("careful-hatchback-74"));
     }
+
+    @Test
+    public void should_server_return_optimization_when_asked_whispering_putter_25() throws Exception {
+        should_server_return_optimization_when_asked(
+                "[{\"VOL\":\"whispering-putter-25\",\"DEPART\":0,\"DUREE\":4,\"PRIX\":8},{\"VOL\":\"hungry-hag-6\",\"DEPART\":1,\"DUREE\":2,\"PRIX\":4},{\"VOL\":\"beautiful-vocabulary-10\",\"DEPART\":2,\"DUREE\":6,\"PRIX\":7},{\"VOL\":\"courageous-light-68\",\"DEPART\":4,\"DUREE\":5,\"PRIX\":11},{\"VOL\":\"harsh-ant-9\",\"DEPART\":5,\"DUREE\":2,\"PRIX\":1},{\"VOL\":\"gifted-possum-86\",\"DEPART\":5,\"DUREE\":4,\"PRIX\":15},{\"VOL\":\"creepy-den-66\",\"DEPART\":6,\"DUREE\":2,\"PRIX\":4},{\"VOL\":\"husky-grater-12\",\"DEPART\":7,\"DUREE\":6,\"PRIX\":4},{\"VOL\":\"misty-macaroon-90\",\"DEPART\":9,\"DUREE\":5,\"PRIX\":16},{\"VOL\":\"hurt-lion-93\",\"DEPART\":10,\"DUREE\":2,\"PRIX\":12},{\"VOL\":\"helpful-panther-70\",\"DEPART\":10,\"DUREE\":4,\"PRIX\":15},{\"VOL\":\"helpful-matt-10\",\"DEPART\":11,\"DUREE\":2,\"PRIX\":6},{\"VOL\":\"slow-bath-19\",\"DEPART\":12,\"DUREE\":6,\"PRIX\":1},{\"VOL\":\"enthusiastic-alfalfa-44\",\"DEPART\":14,\"DUREE\":5,\"PRIX\":23},{\"VOL\":\"disturbed-springtime-72\",\"DEPART\":15,\"DUREE\":2,\"PRIX\":17},{\"VOL\":\"great-slyness-7\",\"DEPART\":15,\"DUREE\":4,\"PRIX\":12},{\"VOL\":\"nutty-goatee-75\",\"DEPART\":16,\"DUREE\":2,\"PRIX\":7},{\"VOL\":\"successful-tracer-37\",\"DEPART\":17,\"DUREE\":6,\"PRIX\":1},{\"VOL\":\"husky-tribune-83\",\"DEPART\":19,\"DUREE\":5,\"PRIX\":10},{\"VOL\":\"deep-music-85\",\"DEPART\":20,\"DUREE\":2,\"PRIX\":13},{\"VOL\":\"resonant-scrap-81\",\"DEPART\":20,\"DUREE\":4,\"PRIX\":9},{\"VOL\":\"lively-raise-63\",\"DEPART\":21,\"DUREE\":2,\"PRIX\":1},{\"VOL\":\"alive-ice-34\",\"DEPART\":22,\"DUREE\":6,\"PRIX\":5},{\"VOL\":\"amused-scumbag-75\",\"DEPART\":24,\"DUREE\":5,\"PRIX\":13},{\"VOL\":\"clumsy-surgery-73\",\"DEPART\":25,\"DUREE\":2,\"PRIX\":7},{\"VOL\":\"precious-beaker-18\",\"DEPART\":25,\"DUREE\":4,\"PRIX\":8},{\"VOL\":\"dark-beast-80\",\"DEPART\":26,\"DUREE\":2,\"PRIX\":8},{\"VOL\":\"enchanting-ventilation-28\",\"DEPART\":27,\"DUREE\":6,\"PRIX\":3},{\"VOL\":\"grotesque-polyester-76\",\"DEPART\":29,\"DUREE\":5,\"PRIX\":11},{\"VOL\":\"gentle-eyeglass-86\",\"DEPART\":30,\"DUREE\":2,\"PRIX\":3},{\"VOL\":\"famous-cake-97\",\"DEPART\":30,\"DUREE\":4,\"PRIX\":11},{\"VOL\":\"time-desk-37\",\"DEPART\":31,\"DUREE\":2,\"PRIX\":2},{\"VOL\":\"frightened-scalp-70\",\"DEPART\":32,\"DUREE\":6,\"PRIX\":3},{\"VOL\":\"powerful-knight-25\",\"DEPART\":34,\"DUREE\":5,\"PRIX\":10},{\"VOL\":\"flipped-out-ballet-94\",\"DEPART\":35,\"DUREE\":2,\"PRIX\":11},{\"VOL\":\"condemned-duct-57\",\"DEPART\":35,\"DUREE\":4,\"PRIX\":8},{\"VOL\":\"slow-suit-66\",\"DEPART\":36,\"DUREE\":2,\"PRIX\":9},{\"VOL\":\"whispering-prize-92\",\"DEPART\":37,\"DUREE\":6,\"PRIX\":1},{\"VOL\":\"silent-scholar-41\",\"DEPART\":39,\"DUREE\":5,\"PRIX\":12},{\"VOL\":\"worried-pediatric-74\",\"DEPART\":40,\"DUREE\":2,\"PRIX\":6}]"
+            , 122);
+    }
+
+    @Test
+    public void should_server_return_optimization_when_asked_gleaming_molar_86() throws Exception {
+        should_server_return_optimization_when_asked(
+                "\t[{\"VOL\":\"gleaming-molar-86\",\"DEPART\":0,\"DUREE\":4,\"PRIX\":11},{\"VOL\":\"super-storefront-97\",\"DEPART\":1,\"DUREE\":2,\"PRIX\":10},{\"VOL\":\"nervous-walkway-45\",\"DEPART\":2,\"DUREE\":6,\"PRIX\":1},{\"VOL\":\"obnoxious-mushroom-21\",\"DEPART\":4,\"DUREE\":5,\"PRIX\":13},{\"VOL\":\"upset-butterfly-44\",\"DEPART\":5,\"DUREE\":2,\"PRIX\":8},{\"VOL\":\"grumpy-voyager-1\",\"DEPART\":5,\"DUREE\":4,\"PRIX\":10},{\"VOL\":\"innocent-ballerina-10\",\"DEPART\":6,\"DUREE\":2,\"PRIX\":3},{\"VOL\":\"flat-wisdom-53\",\"DEPART\":7,\"DUREE\":6,\"PRIX\":6},{\"VOL\":\"black-bugle-48\",\"DEPART\":9,\"DUREE\":5,\"PRIX\":14},{\"VOL\":\"smiling-senselessness-26\",\"DEPART\":10,\"DUREE\":2,\"PRIX\":2},{\"VOL\":\"combative-custard-10\",\"DEPART\":10,\"DUREE\":4,\"PRIX\":14},{\"VOL\":\"resonant-herb-45\",\"DEPART\":11,\"DUREE\":2,\"PRIX\":2},{\"VOL\":\"tense-pilgrim-83\",\"DEPART\":12,\"DUREE\":6,\"PRIX\":7},{\"VOL\":\"zealous-wasp-12\",\"DEPART\":14,\"DUREE\":5,\"PRIX\":6},{\"VOL\":\"tiny-mathematics-52\",\"DEPART\":15,\"DUREE\":2,\"PRIX\":19},{\"VOL\":\"beautiful-sportswoman-85\",\"DEPART\":15,\"DUREE\":4,\"PRIX\":11},{\"VOL\":\"smiling-cerebellum-51\",\"DEPART\":16,\"DUREE\":2,\"PRIX\":2},{\"VOL\":\"troubled-goalie-66\",\"DEPART\":17,\"DUREE\":6,\"PRIX\":6},{\"VOL\":\"teeny-tiny-gourmet-21\",\"DEPART\":19,\"DUREE\":5,\"PRIX\":16},{\"VOL\":\"average-cursor-88\",\"DEPART\":20,\"DUREE\":2,\"PRIX\":4},{\"VOL\":\"bewildered-snap-5\",\"DEPART\":20,\"DUREE\":4,\"PRIX\":8},{\"VOL\":\"fantastic-rule-65\",\"DEPART\":21,\"DUREE\":2,\"PRIX\":3},{\"VOL\":\"fancy-advisor-19\",\"DEPART\":22,\"DUREE\":6,\"PRIX\":5},{\"VOL\":\"squealing-myth-86\",\"DEPART\":24,\"DUREE\":5,\"PRIX\":22},{\"VOL\":\"great-sphere-59\",\"DEPART\":25,\"DUREE\":2,\"PRIX\":17},{\"VOL\":\"vivacious-dealer-14\",\"DEPART\":25,\"DUREE\":4,\"PRIX\":6},{\"VOL\":\"tall-winter-70\",\"DEPART\":26,\"DUREE\":2,\"PRIX\":8},{\"VOL\":\"brave-gas-43\",\"DEPART\":27,\"DUREE\":6,\"PRIX\":1},{\"VOL\":\"sore-speckle-65\",\"DEPART\":29,\"DUREE\":5,\"PRIX\":5},{\"VOL\":\"gorgeous-klutz-96\",\"DEPART\":30,\"DUREE\":2,\"PRIX\":22}]"
+            , 117);
+    }
+
+    @Test
+    public void should_server_return_optimization_when_asked_shallow_kidnapper_93() throws Exception {
+        should_server_return_optimization_when_asked(
+                "\t[{\"VOL\":\"shallow-kidnapper-93\",\"DEPART\":0,\"DUREE\":4,\"PRIX\":10},{\"VOL\":\"easy-tableware-84\",\"DEPART\":1,\"DUREE\":2,\"PRIX\":7},{\"VOL\":\"clean-vista-85\",\"DEPART\":2,\"DUREE\":6,\"PRIX\":6},{\"VOL\":\"high-termite-66\",\"DEPART\":4,\"DUREE\":5,\"PRIX\":21},{\"VOL\":\"fragile-notch-18\",\"DEPART\":5,\"DUREE\":2,\"PRIX\":27},{\"VOL\":\"quiet-blackjack-43\",\"DEPART\":5,\"DUREE\":4,\"PRIX\":13},{\"VOL\":\"disturbed-cloak-27\",\"DEPART\":6,\"DUREE\":2,\"PRIX\":5},{\"VOL\":\"rich-bassist-15\",\"DEPART\":7,\"DUREE\":6,\"PRIX\":5},{\"VOL\":\"defeated-ripple-6\",\"DEPART\":9,\"DUREE\":5,\"PRIX\":7},{\"VOL\":\"zealous-wardroom-14\",\"DEPART\":10,\"DUREE\":2,\"PRIX\":14},{\"VOL\":\"great-tango-28\",\"DEPART\":10,\"DUREE\":4,\"PRIX\":13},{\"VOL\":\"wild-skunk-99\",\"DEPART\":11,\"DUREE\":2,\"PRIX\":7},{\"VOL\":\"blue-eyed-varnish-11\",\"DEPART\":12,\"DUREE\":6,\"PRIX\":6},{\"VOL\":\"poised-spareribs-13\",\"DEPART\":14,\"DUREE\":5,\"PRIX\":4},{\"VOL\":\"annoying-numismatist-69\",\"DEPART\":15,\"DUREE\":2,\"PRIX\":9},{\"VOL\":\"excited-popcorn-90\",\"DEPART\":15,\"DUREE\":4,\"PRIX\":14},{\"VOL\":\"quiet-actress-30\",\"DEPART\":16,\"DUREE\":2,\"PRIX\":5},{\"VOL\":\"thoughtless-floor-31\",\"DEPART\":17,\"DUREE\":6,\"PRIX\":2},{\"VOL\":\"bad-skunk-19\",\"DEPART\":19,\"DUREE\":5,\"PRIX\":7},{\"VOL\":\"odd-stowaway-80\",\"DEPART\":20,\"DUREE\":2,\"PRIX\":30},{\"VOL\":\"better-grain-97\",\"DEPART\":20,\"DUREE\":4,\"PRIX\":11},{\"VOL\":\"encouraging-meadowlark-20\",\"DEPART\":21,\"DUREE\":2,\"PRIX\":8},{\"VOL\":\"cheerful-violoncello-32\",\"DEPART\":22,\"DUREE\":6,\"PRIX\":2},{\"VOL\":\"fancy-throat-62\",\"DEPART\":24,\"DUREE\":5,\"PRIX\":14},{\"VOL\":\"splendid-sawdust-43\",\"DEPART\":25,\"DUREE\":2,\"PRIX\":13}]"
+            , 109);
+    }
+
+    public void should_server_return_optimization_when_asked(String request, int expectedGain) throws Exception {
+        ClientResponse<String> response = server.request("/jajascript/optimize")
+                .body("application/json", request.getBytes("UTF-8")).post(String.class);
+
+        assertThat(response.getResponseStatus().getStatusCode(), is(201));
+
+        JsonNode json = new ObjectMapper().readTree(response.getEntity());
+        assertThat(json, is(notNullValue()));
+        assertThat(json.isObject(), is(true));
+
+        assertThat(json.get("gain").asInt(), is(expectedGain));
+    }
+
+
 }
